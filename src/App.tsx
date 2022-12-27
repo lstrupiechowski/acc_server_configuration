d6156c48e5727b8eec53ddb1415269456f2a0835
strupiechowski.lukasz <strupiechowski.lukasz@gmail.com>
2022-12-27 12:30:29 +0100

    some improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Main from "./Components/Main";
 
 function App() {
+
+  useEffect(() => {
+    document.title = "ACC driver swap configurator - PS5 server"
+  }, []);
   return (
     <Main />
   );
